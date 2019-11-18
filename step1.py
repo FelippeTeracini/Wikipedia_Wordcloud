ebe470f6936c2eb7a11ac0f1e91d59e767449592
TeraByte900 <fe.n.teracini@hotmail.com>
2019-11-18 10:33:11 -0300

    update steps

--- step1.py
+++ step1.py
@@ -4,8 +4,9 @@ from os import environ
 
 spark = SparkSession.builder.getOrCreate()
 
-spark.read.format('xml').options(rowTag='page').load('s3://brubs-c/data/teste.xml') \
+spark.read.format('xml').options(rowTag='page').load('s3://brubs-c/data/wiki.xml') \
     .select('revision.text._VALUE') \
     .write \
     .option("header", "true") \
-    .parquet("s3://brubs-c/data/teste_parquet")
+    .parquet("s3://brubs-c/data/main_parquet")
+
--- step2.py
+++ step2.py
@@ -1,3 +1,4 @@
+
 from pprint import pprint
 from pyspark.sql import SparkSession
 from math import log10
@@ -7,7 +8,7 @@ import re
 
 spark = SparkSession.builder.getOrCreate()
 
-rdd = spark.read.format('parquet').load('s3://brubs-c/data/teste_parquet').rdd.map(tuple)
+rdd = spark.read.format('parquet').load('s3://brubs-c/data/main_parquet').rdd.map(tuple)
 
 def cut_words(text):
     text_ready = str(text[0])
@@ -18,13 +19,12 @@ n_docs = rdd.count()
 res = rdd.map(cut_words) \
     .flatMap(lambda x: [(k, 1) for k in set(x)]) \
     .reduceByKey(lambda x, y: x + y) \
-    .filter(lambda x: x[1] > 20) \
     .map(lambda x: (x[0], log10(n_docs/x[1]))) \
     .collect()
 
-with open ('idf.pickle', 'wb') as f:
+with open ('main_idf.pickle', 'wb') as f:
     pickle.dump(res, f)
 
-command = 'aws s3 cp idf.pickle s3://brubs-c/data/'
+command = 'aws s3 cp main_idf.pickle s3://brubs-c/data/'
 os.system(command)
 
