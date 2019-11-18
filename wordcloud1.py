e0a7fc621589874af6230b5557ca271a21bfd42c
Felippe Nalin Teracini <fe.n.teracini@hotmail.com>
2019-11-18 05:33:53 -0800

    wordcloud

---
+++ wordcloud1.py
@@ -0,0 +1,14 @@
+import pickle
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+with open('teste_final.pickle', 'rb') as f:
+    word_dict = dict(pickle.load(f))
+
+
+print(word_dict.keys())
+word = str(input("Word: "))
+wc = WordCloud()
+wc.generate_from_frequencies(word_dict[word])
+plt.imshow(wc)
+plt.show()
